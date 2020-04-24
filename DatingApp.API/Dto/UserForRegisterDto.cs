46430300a1f3971fb0acfa8bd7de2204675792cb
vivek <visviv@gmail.com>
2020-04-24 09:52:08 +0530

    First Whole Commit-end of section 4

---
+++ DatingApp.API/Dto/UserForRegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Dto
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(8,MinimumLength =4,ErrorMessage ="You must specify a password between 4 and 8 characters")]
+        public string Password { get; set; }
+
+    }
+}
---
+++ DatingAppSPA/src/app/_services/auth.service.ts
@@ -0,0 +1,28 @@
+
+// This is a service in Angular-- create service
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from 'rxjs/operators';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+baseUrl='https://localhost:44342/api/auth/';
+constructor(private http: HttpClient) { }
+
+
+login(model: any){
+return this.http.post(this.baseUrl + 'login', model).pipe(
+  map((response: any) => {
+    const user = response;
+    if(user){
+      localStorage.setItem('token', user.token);
+    }
+  })
+);
+}
+register(model:any){
+  return this.http.post(this.baseUrl+'register',model);
+}
+
+}
